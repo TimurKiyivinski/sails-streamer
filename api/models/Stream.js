3606efe605934ec7498efbae0e560c5a751dc26c
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2016-02-12 23:08:37 +0800

    Initial works

---
+++ api/models/Stream.js
@@ -0,0 +1,17 @@
+/**
+* Stream.js
+*
+* @description :: Holds the URL for every single frame in the stream
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    url: {
+      type: 'string'
+    }
+
+  }
+};
+
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
 (function() {
     console.log("Hello Sails!");
+    var i = 1;
+    
+    window.setInterval(function(){
+        i++;
+        $('#streamer').attr('src', $('#streamer').data('url') + "/images/img" + i + ".jpg");
+        if ( i == 1142 ) i = 1;
+    }, 50);
+
 })();
