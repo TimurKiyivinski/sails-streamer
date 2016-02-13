6f4cf1e42e96ff87de4ee031a165f0e9e73d522b
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2016-02-13 12:08:39 +0800

    Make code more flexible.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,36 @@
 (function() {
-    console.log("Hello Sails!");
-    var i = 1;
+    var getLatest = function() {
+        // Streamer objects
+        var streamer = $('#streamer');
+        var screenshot = $('#screenshot');
+        
+        // Exit if paused
+        if (streamer.data('play') == 0) return;
+        
+        // Increment frame
+        var frame = streamer.data('frame') + 1;
+        streamer.data('frame', frame);
+        
+        // Crete frame URL
+        var src = streamer.data('url') + "/images/frame-" + frame + ".png"
+        
+        // Apply frame
+        streamer.attr('src', src);
+        screenshot.attr('href', src);
+        
+        // Reset on final frame
+        if (frame == streamer.data('end'))
+            streamer.data('frame', streamer.data('start'));
+    }
+    
+    $('#toggle').click(function() {
+        // Streamer objects
+        var streamer = $('#streamer');
+        streamer.data('play') == 0 ? streamer.data('play', 1) : streamer.data('play', 0);
+    });
     
     window.setInterval(function(){
-        i++;
-        $('#streamer').attr('src', $('#streamer').data('url') + "/images/img" + i + ".jpg");
-        if ( i == 1142 ) i = 1;
-    }, 50);
+        getLatest();
+    }, 10);
 
 })();
