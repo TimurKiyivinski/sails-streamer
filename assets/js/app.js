5946589206b4a9a73e76812a322b27ad60a85e50
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2016-03-10 22:55:25 +0800

    More testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,39 @@
 (function() {
     var getLatest = function() {
         // Streamer objects
-        var streamer = $('#streamer');
+        var streamers = $('.streamer');
         var screenshot = $('#screenshot');
         
-        // Exit if paused
-        if (streamer.data('play') == 0) return;
-        
-        // Increment frame
-        var frame = streamer.data('frame') + 1;
-        streamer.data('frame', frame);
-        
-        // Crete frame URL
-        var src = streamer.data('url') + "/images/frame-" + frame + ".png"
-        
-        // Apply frame
-        streamer.attr('src', src);
-        screenshot.attr('href', src);
-        
-        // Reset on final frame
-        if (frame == streamer.data('end'))
-            streamer.data('frame', streamer.data('start'));
+        streamers.each(function(index, object) {
+            var streamer = $(this);
+            // Exit if paused
+            if (streamer.data('play') == 0) return;
+
+            // Increment frame
+            var frame = streamer.data('frame') + 1;
+            streamer.data('frame', frame);
+
+            // Crete frame URL
+            var src = streamer.data('url') + "/images/frame-" + frame + ".png"
+
+            // Apply frame
+            streamer.attr('src', src);
+            screenshot.attr('href', src);
+
+            // Reset on final frame
+            if (frame == streamer.data('end'))
+                streamer.data('frame', streamer.data('start'));
+        });
     }
     
     $('#toggle').click(function() {
         // Streamer objects
-        var streamer = $('#streamer');
+        var streamer = $('.streamer');
         streamer.data('play') == 0 ? streamer.data('play', 1) : streamer.data('play', 0);
     });
     
     window.setInterval(function(){
         getLatest();
-    }, 600);
+    }, 60);
 
 })();
